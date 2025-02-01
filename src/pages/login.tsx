b5c248da19d2cb6f7a17afa11b439776e5caca24
Hellium <drakenz2010@gmail.com>
2025-02-02 01:35:07 +0200

    feature/Add Simple Login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,55 @@
-import React from "react";
-
+import { useEffect, useState } from "react";
+import InputField from "../components/input_field";
+import { auth } from "../firebase";
+import { AuthError, signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { useUserStore } from "../stores/user_store";
+import { toast } from "react-toastify";
 const Login: React.FC = () => {
-    return <div className="">login</div>;
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    useEffect(() => {
+        const user = useUserStore.getState().user;
+        if(user) {
+            toast.success("Already Logged In");
+            navigate("/home");
+        }
+    }, [navigate])
+
+    const handleLogin = async(e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password).then((userCredintials) => {
+            if(userCredintials) {
+                useUserStore.getState().setUser(userCredintials.user);
+                toast.success("Successfully Logged In");
+                navigate("/");
+            }
+        }).catch((error) => {
+            if((error as AuthError).code === "auth/wrong-password") {
+                toast.error("Wrong Email or Password");
+            }
+            else {
+                toast.error("Something went wrong");
+            }
+        })
+    }
+    return (
+        <div className="h-screen w-screen flex flex-col items-center justify-center">
+            <InputField
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <InputField
+                placeholder="Password"
+                obsecure
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>
+                Login
+            </button>
+        </div>
+    );
 };
 
 export default Login;
